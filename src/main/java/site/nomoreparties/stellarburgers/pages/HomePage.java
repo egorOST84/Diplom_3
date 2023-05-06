3b5f83c5c6dd161e3a7fe2cb2d7c4b1652460967
astapenka.y <astapenkay@yandex.com>
2023-05-06 18:41:09 +0200

    HomePage POM: added constructor elements and methods to interact with them

--- src/main/java/site/nomoreparties/stellarburgers/pages/HomePage.java
+++ src/main/java/site/nomoreparties/stellarburgers/pages/HomePage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 import site.nomoreparties.stellarburgers.config.AppConfig;
 import site.nomoreparties.stellarburgers.pages.components.HeaderMenuComponent;
 
-import java.util.List;
+import static org.junit.Assert.assertTrue;
 
 public class HomePage extends Page {
     private final WebDriver driver;
@@ -23,8 +23,22 @@ public class HomePage extends Page {
     private WebElement loginButton;
     @FindBy(xpath = ".//button[text()='Оформить заказ']")
     private WebElement orderButton;
+
+    // Элементы меню "Соберите бургер"
     @FindBy(xpath = ".//span[text()='Булки']")
     private WebElement bunsTab;
+    @FindBy(xpath = ".//span[text()='Соусы']")
+    private WebElement saucesTab;
+    @FindBy(xpath = ".//span[text()='Начинки']")
+    private WebElement fillingsTab;
+
+    // Секции меню "Соберите бургер"
+    @FindBy(xpath = ".//div[@Class='BurgerIngredients_ingredients__menuContainer__Xu3Mo']/h2[1]")
+    private WebElement bunsSection;
+    @FindBy(xpath = ".//div[@Class='BurgerIngredients_ingredients__menuContainer__Xu3Mo']/h2[2]")
+    private WebElement saucesSection;
+    @FindBy(xpath = ".//div[@Class='BurgerIngredients_ingredients__menuContainer__Xu3Mo']/h2[3]")
+    private WebElement fillingsSection;
 
     public static HomePage open(WebDriver driver, String url) {
         driver.get(url);
@@ -43,6 +57,38 @@ public class HomePage extends Page {
             return new LoginPage(driver);
         }
         return new AccountPage(driver);
+    }
+
+
+
+    public void goToBunsTab() {
+        // Переход к разделу "Соусы" так как таю "Булки" некликабельный по умолчанию
+        goToSaucesTab();
+        // Нажимаем на таб "Булки"
+        if(isElementClickable(bunsTab)) {
+            bunsTab.click();
+        }
+        // Проверяем, что видна секция с булками
+        assertTrue(isElementVisible(bunsSection));
+    }
+
+    public void goToSaucesTab() {
+        // Нажимаем на таб "Соусы"
+        if(isElementClickable(saucesTab)) {
+            saucesTab.click();
+        }
+        // Проверяем, что видна секция с соусами
+        assertTrue(isElementVisible(saucesSection));
+
+    }
+
+    public void goToFillingsTab() {
+        // Нажимаем на таб "Начинки"
+        if(isElementClickable(fillingsTab)) {
+            fillingsTab.click();
+        }
+        // Проверяем, что видна секция с начинками
+        assertTrue(isElementVisible(fillingsSection));
     }
 }
 
