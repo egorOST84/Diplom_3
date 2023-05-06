b7732a411585438c21fd129715051457a2b15419
astapenka.y <astapenkay@yandex.com>
2023-05-06 16:57:53 +0200

    POM Page: delete unnecessary code, optimize imports

--- src/main/java/site/nomoreparties/stellarburgers/pages/Page.java
+++ src/main/java/site/nomoreparties/stellarburgers/pages/Page.java
@@ -6,15 +6,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import site.nomoreparties.stellarburgers.behavior.signin.SignInBehavior;
-import site.nomoreparties.stellarburgers.behavior.signup.SignUpBehavior;
 import site.nomoreparties.stellarburgers.config.WebDriverConfig;
 
 import java.lang.reflect.Constructor;
 import java.time.Duration;
 import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -23,8 +20,6 @@ public abstract class Page {
     public Page(WebDriver driver) {
         this.driver = driver;
     }
-    SignUpBehavior signUpBehavior;
-    SignInBehavior signInBehavior;
 
     private WebElement waitForElement(WebElement element) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(WebDriverConfig.WAIT_OF_SECONDS_TIMEOUT));
@@ -68,33 +63,12 @@ public abstract class Page {
         }
     }
 
-    public <T> T verifyUrl(Class<T> pageClass, String expectedUrl) {
-        String actualUrl = driver.getCurrentUrl();
-        Assert.assertEquals("Current URL is not as expected.", expectedUrl, actualUrl);
-        try {
-            Constructor<T> constructor = pageClass.getConstructor(WebDriver.class);
-            return constructor.newInstance(driver);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to create instance of page class", e);
-        }
-    }
-
-    public void waitUntilElementsAreVisible(List<WebElement> elements) {
+    public void checkElementsAreVisible(List<WebElement> elements) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(WebDriverConfig.WAIT_OF_SECONDS_TIMEOUT));
-      //  wait.until(ExpectedConditions.visibilityOfAllElements(elements));
 
         for (WebElement element : elements) {
                 assertTrue(element.isDisplayed());
         }
-
-//        for (WebElement element : elements) {
-//            wait.until(ExpectedConditions.visibilityOf(element));
-//            if (!element.isDisplayed()) {
-//                return false;
-//            }
-//        }
-//
-//        return true;
     }
 
     public void verifyInputValue(WebElement inputElement, String value){
@@ -104,23 +78,4 @@ public abstract class Page {
             throw new IllegalArgumentException("Invalid web element: " + inputElement);
         }
     }
-
-//    public LoginPage goToLoginPage() {
-//        return new LoginPage(driver);
-//    }
-//    public <T> T signIn(Class<T> pageClass, String url) throws NoSuchMethodException {
-//        openPage(pageClass, url);
-//        return getPageInstance(pageClass);
-//    }
-//
-//
-//
-//    public <T> T getPageInstance(Class<T> pageClass) {
-//        try {
-//            Constructor<T> constructor = pageClass.getConstructor(WebDriver.class);
-//            return constructor.newInstance(driver);
-//        } catch (Exception e) {
-//            throw new RuntimeException("Failed to create instance of page class", e);
-//        }
-//    }
 }
