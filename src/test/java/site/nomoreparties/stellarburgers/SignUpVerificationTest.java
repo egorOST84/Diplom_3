bf4fb68db69427f47a95479553e4e51c7e23677d
astapenka.y <astapenkay@yandex.com>
2023-05-06 15:32:05 +0200

    Tests: added SignUpVerificationTest

---
+++ src/test/java/site/nomoreparties/stellarburgers/SignUpVerificationTest.java
@@ -0,0 +1,46 @@
+package site.nomoreparties.stellarburgers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import site.nomoreparties.stellarburgers.config.AppConfig;
+import site.nomoreparties.stellarburgers.config.RndConf;
+import site.nomoreparties.stellarburgers.config.RndStr;
+import site.nomoreparties.stellarburgers.extensions.WebDriverFactory;
+import site.nomoreparties.stellarburgers.pages.LoginPage;
+import site.nomoreparties.stellarburgers.pages.SignupPage;
+
+public class SignUpVerificationTest {
+    private static WebDriver driver;
+
+    @Before
+    public void setup() {
+        driver = WebDriverFactory.get();
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void checkSignUpAndVerifyAccountData() {
+        // Генерируем случайные данные для регистрации пользователя
+        String name = new RndStr().get(RndConf.NAME, 15);
+        String email = new RndStr().get(RndConf.EMAIL, 15);
+        String password = new RndStr().get(RndConf.PASS, 15);
+        // Открываем страницу регистрации и регистрируем пользователя
+        SignupPage.open(driver, AppConfig.SIGNUP_URL)
+                .signUp(name, email, password)
+                // Ждем пока страница загрузится и переходим на страницу логина
+                .waitUntilPageIsLoaded(LoginPage.class, AppConfig.LOGIN_URL)
+                // входим в систему
+                .signIn(email, password)
+        // Открываем главную страницу
+        //HomePage.open(driver, AppConfig.BASE_URL)
+                // переходим в личный кабинет и проверяем верность данных
+                .clickAccountProfileLinkToHeader();
+        //TODO AccountPage.open(driver, AppConfig.ACCOUNT_PROFILE_URL).verifyAccountProfileData(name, email);
+    }
+}
