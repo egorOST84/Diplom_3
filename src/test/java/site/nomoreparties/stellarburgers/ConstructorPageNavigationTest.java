1316e3bf75e0210f999a55edefb62c793ec788ae
astapenka.y <astapenkay@yandex.com>
2023-05-06 18:46:46 +0200

    Tests: updated ConstructorPageNavigationTest - added 3 tests, refactoring

--- src/test/java/site/nomoreparties/stellarburgers/ConstructorPageNavigationTest.java
+++ src/test/java/site/nomoreparties/stellarburgers/ConstructorPageNavigationTest.java
@@ -2,30 +2,12 @@ package site.nomoreparties.stellarburgers;
 
 import org.junit.*;
 import org.openqa.selenium.WebDriver;
-import site.nomoreparties.stellarburgers.api.UserApiHelper;
 import site.nomoreparties.stellarburgers.config.AppConfig;
-import site.nomoreparties.stellarburgers.config.RndConf;
-import site.nomoreparties.stellarburgers.config.RndStr;
 import site.nomoreparties.stellarburgers.extensions.WebDriverFactory;
-import site.nomoreparties.stellarburgers.pages.AccountPage;
 import site.nomoreparties.stellarburgers.pages.HomePage;
-import site.nomoreparties.stellarburgers.pages.LoginPage;
 
 public class ConstructorPageNavigationTest {
     private static WebDriver driver;
-    private static String email;
-    private static String name;
-    private static String password;
-
-    @BeforeClass
-    public static void createUser(){
-        // Генерируем случайные данные для регистрации пользователя
-        name = new RndStr().get(RndConf.NAME, 15);
-        email = new RndStr().get(RndConf.EMAIL, 15);
-        password = new RndStr().get(RndConf.PASS, 15);
-        // Создаем пользователя для тестов
-        UserApiHelper.createUser(name, email, password);
-    }
     @Before
     public void setup() {
         driver = WebDriverFactory.get();
@@ -36,22 +18,27 @@ public class ConstructorPageNavigationTest {
         driver.quit();
     }
 
-    @AfterClass
-    public static void deleteUser() {
-        String accessToken = UserApiHelper.loginUserAndGetToken(email, password);
-        UserApiHelper.deleteUser(accessToken);
+    @Test
+    public void checkBunsNavigation() {
+        //Нажали на элемент "Булки" и проверили, что открылась соответствующая страница с булками.
+        HomePage.open(driver, AppConfig.BASE_URL)
+                .waitUntilPageIsLoaded(HomePage.class, AppConfig.BASE_URL)
+                .goToBunsTab();
     }
 
     @Test
-    public void checkBunsNavigation(){
-
+    public void checkSaucesNavigation() {
+        //Нажали на элемент "Соусы" и проверили, что открылась соответствующая страница с соусами.
+        HomePage.open(driver, AppConfig.BASE_URL)
+                .waitUntilPageIsLoaded(HomePage.class, AppConfig.BASE_URL)
+                .goToSaucesTab();
     }
-    @Test
-    public void checkSaucesNavigation(){
 
-    }
     @Test
-    public void checkFillingsNavigation(){
-
+    public void checkFillingsNavigation() {
+        //Нажали на элемент "Начинки" и проверили, что открылась соответствующая страница с начинками.
+        HomePage.open(driver, AppConfig.BASE_URL)
+                .waitUntilPageIsLoaded(HomePage.class, AppConfig.BASE_URL)
+                .goToFillingsTab();
     }
 }
